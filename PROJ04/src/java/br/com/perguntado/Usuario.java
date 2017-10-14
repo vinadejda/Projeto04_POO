d4939360ec7f4d5907ffb7d1464c77b5a58d6e24
Laércio <Larcio@Home>
2017-10-13 21:14:51 -0300

    Session- Vitoria Sistema de Perguntas - Rodrigo

--- PROJ04/src/java/br/com/perguntado/Usuario.java
+++ PROJ04/src/java/br/com/perguntado/Usuario.java
@@ -1,71 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.perguntado;
-
 import java.util.ArrayList;
-
 /**
  *
- * @author Laércio
+ * @author vitoria
  */
 public class Usuario {
-    private String nome;
-    private String login;
-    private double media;
+   private String nome;
+   
+       /*Construtor de Usuarios*/
     
-    
-    /*Construtor de Usuarios*/
-    
-    public Usuario(String nome, String login){
+    public Usuario(String nome){
         this.nome = nome;
-        this.login = login;
-        /*A media começa com 0*/
-        this.media = 0;
     }
-
+   
+   public static ArrayList<Usuario> listUsuarios;
+   
+   public static ArrayList<Usuario> getListUsuarios(){
+        if(listUsuarios==null){
+            listUsuarios = new ArrayList<>();
+            Usuario u1 = new Usuario("Luciana");
+            listUsuarios.add(u1);
+            
+            Usuario u2 =new Usuario("Luiz");
+            listUsuarios.add(u2);
+            
+            Usuario u3 =new Usuario("Rodrigo");
+            listUsuarios.add(u3);
+            
+            Usuario u4 =new Usuario("Vitoria");
+            listUsuarios.add(u4);
+        }
+        return listUsuarios;
+   }
+   
     public String getNome() {
         return nome;
     }
-
+    
     public void setNome(String nome) {
         this.nome = nome;
     }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public double getMedia() {
-        return media;
-    }
-
-    public void setMedia(double media) {
-        this.media = media;
-    }
     
-    /*Metódo de cadastro dos usuarios no arrayList*/
-    public static ArrayList<Usuario> usuarios;
-    public static ArrayList<Usuario> getUsu(){
-    /*Instanciando arrayList de usuarios*/
-    usuarios = new ArrayList<>();
-    /*Lista de usuarios já cadastrados. Usuarios Default*/
-    Usuario rodrigo = new Usuario("Rodrigo Garcia","rodrigo");
-    Usuario luiz = new Usuario("Luiz Henrique","luiz");
-
-    
-    /*adicionando USUARIOS ao arrayList de usuarios*/       
-    usuarios.add(rodrigo);
-    usuarios.add(luiz); 
-    
-    /*RETORNANDO ARRAYLIST DE USUARIOS*/
-    return usuarios;
-    }
-    
-}
+   }
